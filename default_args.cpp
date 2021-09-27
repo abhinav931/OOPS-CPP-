2b6d3cf56fa35c238eb6979dc9ed0cc9fd9e4560
abhinav <vabhinav182@gmail.com>
2021-09-27 06:54:39 +0530

    default arguments

---
+++ default_args.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+//default arguments should be at the end of arg list, since, only that way compiler can map the variables, call --> definition
+int sum(int, int, int = 0);
+
+int main()
+{
+    int a = 2, b = 3;
+    cout << sum(2, 3);
+}
+
+int sum(int a, int b, int c)
+{
+    cout << c << endl; //c=0, eventhought not passed in func call, since we already set default value of c in func definition.
+
+    return a + b + c;
+}
